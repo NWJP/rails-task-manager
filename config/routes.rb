70900ab28063d2b32045b187251aba74c257f55b
Nakia Wilson <n.wil@workmail.com>
2024-06-29 20:18:02 +0900

    ch.2

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/tasks", to: "tasks#index", as: :tasks
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  post "/tasks", to: "tasks#create", as: :create_task
+  get "tasks/:id", to: "tasks#show", as: :task
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
